03e42ce1aff148fdd63066ce71d902479a3b52b1
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-10-29 20:56:00 -0300

    Implemented conversion methods

---
+++ RestWithAspNetAPI/RestWithAspNetAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithAspNetAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber,string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDouble(firstNumber) + ConvertToDouble(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private double ConvertToDouble(string strNumber)
+        {
+            double doubleValue;
+            if (double.TryParse(strNumber, out doubleValue))
+                return doubleValue;
+            return 0.0;
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(
+                strNumber,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out number);
+            return isNumber;
+        }
+    }
+}
