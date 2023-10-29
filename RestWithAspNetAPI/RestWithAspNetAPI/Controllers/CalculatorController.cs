969cc6857b222b89766782052c32b5cc2714d970
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-10-29 20:58:55 -0300

    Update in Calculator routing prefix

--- RestWithAspNetAPI/RestWithAspNetAPI/Controllers/CalculatorController.cs
+++ RestWithAspNetAPI/RestWithAspNetAPI/Controllers/CalculatorController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace RestWithAspNetAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class CalculatorController : ControllerBase
     {
