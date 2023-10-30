2a1226503df56eb5be56254cbc377913ed818551
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-10-29 21:44:42 -0300

    Renamed controller and added model

---
+++ RestWithAspNetAPI/RestWithAspNetAPI/Controllers/PersonController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithAspNetAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("Addition/{firstNumber}/{secondNumber}")]
+        public IActionResult Addition(string firstNumber, string secondNumber)
+        {
+            return BadRequest("Invalid Input");
+        }
+    }
+}
---
+++ RestWithAspNetAPI/RestWithAspNetAPI/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace RestWithAspNetAPI.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? Gender { get; set; }
+    }
+}
