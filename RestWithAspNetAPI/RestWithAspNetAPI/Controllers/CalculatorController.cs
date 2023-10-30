771707658798315896aabd9100b5dfa58f9ca53d
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-10-29 21:30:06 -0300

    Added all the operations

--- RestWithAspNetAPI/RestWithAspNetAPI/Controllers/CalculatorController.cs
+++ RestWithAspNetAPI/RestWithAspNetAPI/Controllers/CalculatorController.cs
@@ -15,13 +15,72 @@ namespace RestWithAspNetAPI.Controllers
             _logger = logger;
         }
 
-        [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult Get(string firstNumber,string secondNumber)
+        [HttpGet("Addition/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Addition(string firstNumber, string secondNumber)
         {
             if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
             {
                 var sum = ConvertToDouble(firstNumber) + ConvertToDouble(secondNumber);
                 return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("Subtraction/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Subtraction(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var result = ConvertToDouble(firstNumber) - ConvertToDouble(secondNumber);
+                return Ok(result.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("Multiplication/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Multiplication(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var result = ConvertToDouble(firstNumber) * ConvertToDouble(secondNumber);
+                return Ok(result.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("Division/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Division(string firstNumber, string secondNumber)
+        {
+            if (secondNumber != "0")
+            {
+                if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+                {
+                    var result = ConvertToDouble(firstNumber); ConvertToDouble(secondNumber);
+                    return Ok(result.ToString());
+                }
+                return BadRequest("Invalid Input");
+            }
+            return BadRequest($"Second number: {secondNumber} must be greater than 0");
+        }
+
+        [HttpGet("Mean/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Mean(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var result = (ConvertToDouble(firstNumber) + ConvertToDouble(secondNumber)) / 2;
+                return Ok(result.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("SquareRoot/{firstNumber}")]
+        public ActionResult<string> SquareRoot(string firstNumber)
+        {
+            if (IsNumeric(firstNumber))
+            {
+                var result = Math.Sqrt(ConvertToDouble(firstNumber));
+                return Ok(result.ToString());
             }
             return BadRequest("Invalid Input");
         }
