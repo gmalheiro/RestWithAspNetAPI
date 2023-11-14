28b4be98919d938c87401f7c5118fafb4f01bfb6
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-11-13 23:27:20 -0300

    Created method to migrate database

---
+++ RestWithAspNetAPI/RestWithAspNetAPI/Data/MigrateDatabase.cs
@@ -0,0 +1,28 @@
+﻿using EvolveDb;
+using MySqlConnector;
+using Serilog;
+
+namespace RestWithAspNetAPI.Data
+{
+    public static class MigrateDatabase 
+    {
+        public static void MigrateDb(string connection)
+        {
+            try
+            {
+                var evolveConnection = new MySqlConnection(connection);
+                var evolve = new Evolve(evolveConnection, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations", "db/dataset" },
+                    IsEraseDisabled = true,
+                };
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Database migration failed", ex);
+                throw;
+            }
+        }
+    }
+}
