f2d0124bee3c439168ffe1816301012bfdff39fb
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-11-15 14:07:23 -0300

    Created IBookRepository

---
+++ RestWithAspNetAPI/RestWithAspNetAPI/Repository/IBookRepository.cs
@@ -0,0 +1,19 @@
+﻿using RestWithAspNetAPI.Models;
+
+namespace RestWithAspNetAPI.Repository
+{
+    public interface IBokRepository
+    {
+        Person Create(Book book);
+
+        Person FindById(long id);
+
+        List<Person> FindAll();
+
+        Person Update(Book book);
+
+        Person Delete(int id);
+
+        bool Exists(long id);
+    }
+}
