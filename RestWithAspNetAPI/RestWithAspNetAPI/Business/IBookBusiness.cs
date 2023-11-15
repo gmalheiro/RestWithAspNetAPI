7fb99ab5da8a2304dbaed71ab5994a9474dae9da
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-11-15 19:27:23 -0300

    IBook business done

---
+++ RestWithAspNetAPI/RestWithAspNetAPI/Business/IBookBusiness.cs
@@ -0,0 +1,17 @@
+﻿using RestWithAspNetAPI.Models;
+
+namespace RestWithAspNetAPI.Business
+{
+    public interface IBookBusiness
+    {
+        Person Create(Book book);
+
+        Person FindById(long id);
+
+        List<Book> FindAll();
+
+        Person Update(Book book);
+
+        Person Delete(int id);    
+    }
+}
