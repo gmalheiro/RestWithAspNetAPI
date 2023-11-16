0124f40b6da620f67ec6ebb2eba8f23c34ed53c3
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-11-15 22:13:24 -0300

    Added IRepository class

---
+++ RestWithAspNetAPI/RestWithAspNetAPI/Repository/Generic/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using RestWithAspNetAPI.Models;
+using RestWithAspNetAPI.Models.Base;
+
+namespace RestWithAspNetAPI.Repository
+{
+    public interface IRepository <T> where T : BaseEntity
+    {
+        T Create(T item);
+
+        T FindById(long id);
+
+        List<T> FindAll();
+
+        T Update(T item);
+
+        T Delete(int id);
+
+        bool Exists(long id);
+    }
+}
