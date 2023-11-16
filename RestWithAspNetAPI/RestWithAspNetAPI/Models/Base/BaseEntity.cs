ea97fa11d5337f8717e4e3fb07bb74af7fafdd43
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-11-15 22:07:29 -0300

    Created base entity class

---
+++ RestWithAspNetAPI/RestWithAspNetAPI/Models/Base/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithAspNetAPI.Models.Base
+{
+    public class BaseEntity
+    {
+        [Column("id")]
+        public long Id { get; set; }
+    }
+}
