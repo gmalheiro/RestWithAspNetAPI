e341e13a0425e8afa46cb7708c3769e4cec22849
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-11-16 23:04:16 -0300

    Implemented Generic repository

---
+++ RestWithAspNetAPI/RestWithAspNetAPI/Repository/Generic/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using RestWithAspNetAPI.Models.Base;
+
+namespace RestWithAspNetAPI.Repository.Generic
+{
+    public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        public T Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
