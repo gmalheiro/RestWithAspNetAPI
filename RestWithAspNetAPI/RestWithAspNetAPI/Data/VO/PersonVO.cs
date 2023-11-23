0f239b830dcb3f9da64abb5ecfb23028f6e6229c
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-11-23 19:51:20 -0300

    Created PersonVO

---
+++ RestWithAspNetAPI/RestWithAspNetAPI/Data/VO/PersonVO.cs
@@ -0,0 +1,21 @@
+﻿using RestWithAspNetAPI.Models.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithAspNetAPI.Models
+{
+    [Table("person")]
+    public class Person : BaseEntity
+    {
+        [Column("first_name")]
+        public string? FirstName { get; set; }
+
+        [Column("last_name")]
+        public string? LastName { get; set; }
+
+        [Column("address")]
+        public string? Address { get; set; }
+
+        [Column("gender")]
+        public string? Gender { get; set; }
+    }
+}
