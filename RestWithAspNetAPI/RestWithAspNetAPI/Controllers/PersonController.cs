947ef1aa19307fc531cef9af8efa9fd332ab7603
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-12-04 19:19:40 -0300

    Update in PersonController

--- RestWithAspNetAPI/RestWithAspNetAPI/Controllers/PersonController.cs
+++ RestWithAspNetAPI/RestWithAspNetAPI/Controllers/PersonController.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using RestWithAspNetAPI.Models;
 using RestWithAspNetAPI.Business;
+using RestWithAspNetAPI.Data.VO;
 
 namespace RestWithAspNetAPI.Controllers
 {
@@ -43,7 +44,7 @@ namespace RestWithAspNetAPI.Controllers
         // Maps POST requests to https://localhost:{port}/api/person/
         // [FromBody] consumes the JSON object sent in the request body
         [HttpPost]
-        public IActionResult Post([FromBody] Person person)
+        public IActionResult Post([FromBody] PersonVO person)
         {
             if (person == null) return BadRequest();
             return Ok(_personBusiness.Create(person));
@@ -52,7 +53,7 @@ namespace RestWithAspNetAPI.Controllers
         // Maps PUT requests to https://localhost:{port}/api/person/
         // [FromBody] consumes the JSON object sent in the request body
         [HttpPut]
-        public IActionResult Put([FromBody] Person person)
+        public IActionResult Put([FromBody] PersonVO person)
         {
             if (person == null) return BadRequest();
             return Ok(_personBusiness.Update(person));
