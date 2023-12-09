dc60acc526b08c6970f77f4107a8d05375e20775
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-12-09 15:17:15 -0300

    Created HyperMediaFilterOptions

---
+++ RestWithAspNetAPI/RestWithAspNetAPI/Hypermedia/Filters/HyperMediaFilterOptions.cs
@@ -0,0 +1,9 @@
+﻿using RestWithAspNetAPI.Hypermedia.Abstract;
+
+namespace RestWithAspNetAPI.Hypermedia.Filters
+{
+    public class HyperMediaFilterOptions
+    {
+        public List<IResponseEnricher> ContentResponseEnricherList { get; set; } = new List<IResponseEnricher>();
+    }
+}
