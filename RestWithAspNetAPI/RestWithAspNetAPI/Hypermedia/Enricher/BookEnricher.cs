bdcdefece95e0d0e59cca9c9a9a27dbdfc754793
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-12-11 15:51:45 -0300

    Update in BookEnricher path

--- RestWithAspNetAPI/RestWithAspNetAPI/Hypermedia/Enricher/BookEnricher.cs
+++ RestWithAspNetAPI/RestWithAspNetAPI/Hypermedia/Enricher/BookEnricher.cs
@@ -9,7 +9,7 @@ namespace RestWithAspNetAPI.Hypermedia.Enricher
     {
         protected override Task EnrichModel(BookVO content, IUrlHelper urlHelper)
         {
-            var path = "api/book";
+            var path = "api/Book";
             string link = GetLink(content.Id, urlHelper, path);
 
             content.Links.Add(new HyperMediaLink()
