f0c6f1ff540d8f1dc7cf54e72d9d29fe0eac6ae4
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-12-11 10:48:44 -0300

    Implemented ISupportsHypermedia in BookVO

--- RestWithAspNetAPI/RestWithAspNetAPI/Data/VO/BookVO.cs
+++ RestWithAspNetAPI/RestWithAspNetAPI/Data/VO/BookVO.cs
@@ -1,11 +1,15 @@
-﻿namespace RestWithAspNetAPI.Data.VO
+﻿using RestWithAspNetAPI.Hypermedia;
+using RestWithAspNetAPI.Hypermedia.Abstract;
+
+namespace RestWithAspNetAPI.Data.VO
 {
-    public class BookVO
+    public class BookVO : ISupportsHyperMedia
     {
         public long Id { get; set; }
         public string? Title { get; set; }
         public string? Author { get; set; }
         public decimal Price { get; set; }
         public DateTime LaunchDate { get; set; }
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
     }
 }
