47d56bb119261d03393cd2b4539a01ecaf39c5fc
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-12-18 20:58:08 -0300

    New field in PersonVO

--- RestWithAspNetAPI/RestWithAspNetAPI/Data/VO/PersonVO.cs
+++ RestWithAspNetAPI/RestWithAspNetAPI/Data/VO/PersonVO.cs
@@ -9,5 +9,6 @@ public class PersonVO : ISupportsHyperMedia
     public string? LastName { get; set; }
     public string? Address { get; set; }
     public string? Gender { get; set; }
+    public bool Enabled { get; set; }
     public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
 }
