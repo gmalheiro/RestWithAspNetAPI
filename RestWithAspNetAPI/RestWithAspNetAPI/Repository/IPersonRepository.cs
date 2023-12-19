784d292c0c19aec23647b71a3a5e0187de0e1259
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-12-19 11:39:01 -0300

    Created FindPersonByName method in repository

--- RestWithAspNetAPI/RestWithAspNetAPI/Repository/IPersonRepository.cs
+++ RestWithAspNetAPI/RestWithAspNetAPI/Repository/IPersonRepository.cs
@@ -5,5 +5,6 @@ namespace RestWithAspNetAPI.Repository
     public interface IPersonRepository : IRepository<Person>
     {
         Person Disable (long id);
+        List<Person> FindByName(string firstName, string secondName);
     }
 }
