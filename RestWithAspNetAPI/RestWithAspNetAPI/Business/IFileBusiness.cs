d602cb8df81800b592fe254ca4d061ea2963567b
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-12-20 15:11:08 -0300

    Created FileBusiness interface

---
+++ RestWithAspNetAPI/RestWithAspNetAPI/Business/IFileBusiness.cs
@@ -0,0 +1,11 @@
+﻿using RestWithAspNetAPI.Data.VO;
+
+namespace RestWithAspNetAPI.Business
+{
+    public interface IFileBusiness
+    {
+        public byte[] GetFile(string filename);
+        public Task<FileDetailVO> SaveFileToDisk(IFormFile file);
+        public Task<List<FileDetailVO>> SaveFilesToDisk(IList<IFormFile> file);
+    }
+}
