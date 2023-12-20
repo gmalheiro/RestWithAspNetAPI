fb72332ac011b5771872f28b82ab25c248ff9ad3
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-12-19 21:44:18 -0300

    Update in ContentResponseEnricher for paged search

--- RestWithAspNetAPI/RestWithAspNetAPI/Hypermedia/ContentResponseEnricher.cs
+++ RestWithAspNetAPI/RestWithAspNetAPI/Hypermedia/ContentResponseEnricher.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc;
 using RestWithAspNetAPI.Hypermedia.Abstract;
 using System.Collections.Concurrent;
+using RestWithAspNetAPI.Hypermedia.Utils;
 
 namespace RestWithAspNetAPI.Hypermedia
 {
@@ -14,7 +15,7 @@ namespace RestWithAspNetAPI.Hypermedia
         }
         public virtual bool CanEnrich(Type contentType)
         {
-            return contentType == typeof(T) || contentType == typeof(List<T>);
+            return contentType == typeof(T) || contentType == typeof(List<T>) || contentType == typeof(PagedSearchVO<T>);
         }
 
         protected abstract Task EnrichModel(T content, IUrlHelper urlHelper);
@@ -40,6 +41,13 @@ namespace RestWithAspNetAPI.Hypermedia
                 {
                     ConcurrentBag<T> bag = new ConcurrentBag<T>(collection);
                     Parallel.ForEach(bag, (element) =>
+                    {
+                        EnrichModel(element, urlHelper);
+                    });
+                }
+                else if (okObjectResult.Value is PagedSearchVO<T> pagedSearch)
+                {
+                    Parallel.ForEach(pagedSearch.List.ToList(), (element) =>
                     {
                         EnrichModel(element, urlHelper);
                     });
