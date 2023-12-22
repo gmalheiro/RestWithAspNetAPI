19314290aa44a09805bf60ecc43c461d541ce06e
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-12-22 00:15:26 -0300

    Update in init_database.sh

--- RestWithAspNetAPI/RestWithAspNetAPI/ci/init_database.sh
+++ RestWithAspNetAPI/RestWithAspNetAPI/ci/init_database.sh
@@ -1,1 +1,1 @@
-for i in `find /home/database/ -name "*.sql" | sort --version-sort`; do mysql -udocker -pdocker rest_with_asp_net_api < $i; done;
+for i in `find /home/database/ -name "*.sql" | sort --version-sort`; do mysql -udocker -pdocker rest_asp_net_udemy < $i; done;
