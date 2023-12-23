b0f2822485550eb93a2a0ad95bac69c055d4040d
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-12-23 15:41:39 -0300

    Changed name to MSSQL

---
+++ RestWithAspNetAPI/RestWithAspNetAPI/Models/Context/MSSQLContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestWithAspNetAPI.Models.Context
+{
+    public class MSSQLContext: DbContext
+    {
+        public MSSQLContext()
+        {
+            
+        }
+
+        public MSSQLContext(DbContextOptions <MSSQLContext> options) :base(options) {}
+
+        public DbSet<Person> Persons { get; set; }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
