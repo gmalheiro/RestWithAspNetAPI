96ed2fceca7b6e48b6c9ec65afe0259704c05c27
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-12-23 14:52:26 -0300

    Update in migration

--- RestWithAspNetAPI/RestWithAspNetAPI/Data/db/migrations/V1__Full_Dump_Postgres.sql
+++ RestWithAspNetAPI/RestWithAspNetAPI/Data/db/migrations/V1__Full_Dump_Postgres.sql
@@ -154,4 +154,4 @@ CREATE TABLE IF NOT EXISTS users (
 )  ;
 
 INSERT INTO users (user_name, password, full_name, refresh_token, refresh_token_expiry_time) VALUES
-	('leandro', '24-0B-E5-18-FA-BD-27-24-DD-B6-F0-4E-EB-1D-A5-96-74-48-D7-E8-31-C0-8C-8F-A8-22-80-9F-74-C7-20-A9', 'LEANDRO DA COSTA GONCALVES', 'F3UV/scxdp134LBEo0Dg6OX0c2lgIgAYSpJ05gSQg/w=', '2020-11-16 12:14:08');
+	('gabriel', '240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9', 'GABRIEL MALHEIRO', 'F3UV/scxdp134LBEo0Dg6OX0c2lgIgAYSpJ05gSQg/w=', '2020-11-16 12:14:08');
