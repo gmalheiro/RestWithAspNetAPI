7fcb1772e7f129fb0c0d1ebeb5d2d2fe62f469a3
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-12-24 11:50:46 -0300

    Update in books

--- client/src/pages/Book/index.js
+++ client/src/pages/Book/index.js
@@ -17,6 +17,30 @@ export default function Login (){
                     <FiPower size={18} color="#251FC5" />
                 </button>
             </header>
+
+            <h1>Registered Books</h1>
+            <ul>
+                <li>
+                    <strong>Title:</strong>
+                    <p>.NET Expert</p>
+                    <strong>Author:</strong>
+                    <p>Gabriel Malheiro</p>
+                    <strong>Price:</strong>
+                    <p>R$ 80,00</p>
+                    <strong>Release Date:</strong>
+                    <p>24/12/2023</p>
+
+                    <button type='button'>
+                        <FiEdit size={20} color='#251FC5'/>
+                    </button>
+
+                    <button type='button'>
+                        <FiTrash2 size={20} color='#251FC5'/>
+                    </button>
+                </li>
+            </ul>
+            
+            
         </div>
     );
 }
