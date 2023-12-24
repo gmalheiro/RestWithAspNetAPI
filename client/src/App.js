9883785b15af8e52956d2e481bb304889d97524d
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-12-24 02:35:04 -0300

    Update in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import Login from "./pages/Login";
 import './global.css';
+import Routes from "./routes";
 
 export default function App() {
   return (
-    <Login/>
+    <Routes/>
   );
 }
