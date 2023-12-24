66aa091bba6893f87496c40100e93af3f656bbb9
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-12-24 17:51:29 -0300

    Update in routes and in book

--- client/src/pages/Book/index.js
+++ client/src/pages/Book/index.js
@@ -12,7 +12,7 @@ export default function Login (){
             <header>
                 <img src={logoImage} alt="Erudio"/>
                 <span>Welcome, <strong>Gabriel</strong>!</span>
-                <Link className="button" to="book/new/0">Add New Book</Link>
+                <Link className="button" to="book/new">Add New Book</Link>
                 <button  type="button">
                     <FiPower size={18} color="#251FC5" />
                 </button>
--- client/src/routes.js
+++ client/src/routes.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route,Switch} from 'react-router-dom';
 
 import Login from './pages/Login';
 import Book from './pages/Book';
+import NewBook from './pages/NewBook';
 
 
 export default function Routes() {
@@ -11,7 +12,8 @@ export default function Routes() {
             <Switch>
                 <Route path="/" exact component={Login}/>
                 <Route path="/login" exact component={Login}/>
-                <Route path="/book" exact component={Book}/>
+                <Route path="/books" exact component={Book}/>
+                <Route path="/book/new" exact component={NewBook}/>
             </Switch>
         </BrowserRouter>
     );
